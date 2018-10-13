2132207dfaf99abeb00ad6394f32ca8ce2e36b9e
alexHooper <alexmohack@gmail.com>
2018-10-13 22:38:31 +0530

    create chatkit instance

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const Chatkit = require('pusher-chatkit-server')
+require('dotenv').config({ silent: true });
+
+const app = express();
+
+const chatkit = new Chatkit.default(require({
+	instanceLocator: process.env.PUSHER_CHATKIT_INSTANCE_LOCATOR,
+	key: process.env.PUSHER_CHATKIT_KEY
+}))
